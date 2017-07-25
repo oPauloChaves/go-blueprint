bba0da615cbc43729a575edfeca7983434aeaf07
Paulo Chaves <opaulochaves@gmail.com>
2017-07-24 23:26:40 -0300

    chat version 1

---
+++ chapter1/chat/client.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/gorilla/websocket"
+)
+
+type client struct {
+	// the websocket for this client
+	socket *websocket.Conn
+	// channel on which the messages are sent
+	send chan []byte
+	// the room this client is chatting in
+	room *room
+}
+
+func (c *client) read() {
+	for {
+		if _, msg, err := c.socket.ReadMessage(); err == nil {
+			c.room.forward <- msg
+		} else {
+			break
+		}
+	}
+	c.socket.Close()
+}
+
+func (c *client) write() {
+	for msg := range c.send {
+		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
+			break
+		}
+	}
+	c.socket.Close()
+}
--- chapter1/chat/main.go
+++ chapter1/chat/main.go
@@ -3,22 +3,31 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>Chat</title>
-				</head>
-				<body>
-					<p>Let's Chat!</p>
-				</body>
-			</html>
-		`))
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template // represents a single template
+}
 
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	r := newRoom()
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	// get the room going
+	go r.run()
 
 	// start the web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
